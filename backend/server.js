e53014af53b6042d82280e395d0d3e70fa826c4a
Oliver Musca <olivermuscajr17@gmail.com>
2025-10-26 23:00:03 +0800

    fixed lowercasse

--- backend/server.js
+++ backend/server.js
@@ -47,9 +47,9 @@ app.get('/api/health', (req, res) => {
 // Import routes
 const productRoutes = require('./routes/ProductRoutes');  // <-- Example fix for capital 'P'
 const categoryRoutes = require('./routes/categoryRoutes'); // <-- Apply to all
-const activityLogRoutes = require('./routes/ActivityLogRoutes');
-const authRoutes = require('./routes/AuthRoutes');
-const reportRoutes = require('./routes/ReportRoutes');
+const activityLogRoutes = require('./routes/activityLogRoutes');
+const authRoutes = require('./routes/authRoutes');
+const reportRoutes = require('./routes/reportRoutes');
 
 // Use routes
 app.use('/api/products', productRoutes);
